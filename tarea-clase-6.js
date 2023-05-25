4565f43f2c7f03da722ba773fb94df9aeef6da37
emiezeoc <emiezeoc@gmail.com>
2023-05-25 00:59:50 -0300

    Agrega funcionamiento de boton siguiente para crear formulario y ocultarlo luego

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -1,4 +1,37 @@
-document.querySelector('#boton-siguiente').onclick = function(){
+const $botonSiguiente = document.querySelector("#boton-siguiente")
+const $inputCantidadIntegrantes = document.querySelector("#cantidad-integrantes")
+
+$botonSiguiente.onclick = function(){
+    const cantidadIntegrantesFamilia = Number($inputCantidadIntegrantes.value)
+    crearFormulario(cantidadIntegrantesFamilia)
+    ocultarBotonSiguiente()
     
+    return cantidadIntegrantesFamilia;
 }
+
+function crearFormulario(cantidadIntegrantesFamilia) {
+    const nodoFormularioEdades = document.querySelector("#formulario-edades")
+    let $labelIntegrante;
+    let $inputEdad;
+    
+    for (let i = 0; i < cantidadIntegrantesFamilia; i++) {
+        
+        $labelIntegrante = document.createElement("label");
+        $labelIntegrante.textContent = `Integrante ${i+1}`;
+        nodoFormularioEdades.appendChild($labelIntegrante);
+
+        $inputEdad = document.createElement("input");
+        $inputEdad.type = "number";
+        nodoFormularioEdades.appendChild($inputEdad);
+    }
+    return false;
+}
+
+function ocultarBotonSiguiente(){
+    $botonSiguiente.className = "oculto"
+    return false;
+}
+
+
+
 
