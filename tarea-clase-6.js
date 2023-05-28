8b16ebd1294fd592a093601bc68c9889ff8fdd12
emiezeoc <emiezeoc@gmail.com>
2023-05-28 00:45:06 -0300

    Agrega acciones del boton calcular, calcula y muestra resultados
    (Falta el codigo para que funcione el boton "resetear")

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -1,5 +1,9 @@
 const $botonSiguiente = document.querySelector("#boton-siguiente")
 const $inputCantidadIntegrantes = document.querySelector("#cantidad-integrantes")
+const $botonCalcular = document.querySelector("#boton-calcular")
+const $botonResetear = document.querySelector("#boton-resetear")
+const $resultados = document.querySelector("#resultados")
+
 
 $botonSiguiente.onclick = function(){
     const cantidadIntegrantesFamilia = Number($inputCantidadIntegrantes.value)
@@ -21,7 +25,9 @@ function crearFormulario(cantidadIntegrantesFamilia) {
         nodoFormularioEdades.appendChild($labelIntegrante);
 
         $inputEdad = document.createElement("input");
+        $inputEdad.className = "edad"
         $inputEdad.type = "number";
+        $inputEdad.placeholder = "Edad"
         nodoFormularioEdades.appendChild($inputEdad);
     }
     return false;
@@ -29,9 +35,44 @@ function crearFormulario(cantidadIntegrantesFamilia) {
 
 function ocultarBotonSiguiente(){
     $botonSiguiente.className = "oculto"
+    $botonCalcular.className = ""
     return false;
 }
 
+$botonCalcular.onclick = function() {
+    const edadesIntegrantes = document.querySelectorAll(".edad")
+    let arrayEdades = []
+    for (let i = 0; i < edadesIntegrantes.length; i++) {
+        arrayEdades.push (Number(edadesIntegrantes[i].value))
+    }
+    calcularResultados(arrayEdades)
+    ocultarBotonCalcular()
 
+    return false;
+}
 
+function calcularResultados(arrayEdades){
+    const mayorEdad = Math.max(...arrayEdades);
+    const menorEdad = Math.min(...arrayEdades);
+    const promedioEdades = promedio(arrayEdades);
 
+    $resultados.textContent = `La mayor edad es ${mayorEdad}, la menor edad es ${menorEdad}, y el promedio de edades es ${promedioEdades}`
+
+    return mayorEdad, menorEdad, promedioEdades;
+}
+
+function promedio(arrayEdades) {
+    let sumatoriaDeEdades = 0;
+    for (let i = 0; i < arrayEdades.length; i++) {
+        sumatoriaDeEdades += arrayEdades[i];        
+    }
+    const promedio = sumatoriaDeEdades / arrayEdades.length
+    
+    return promedio; 
+}
+
+function ocultarBotonCalcular() {
+    $botonCalcular.className = "oculto"
+
+    return false;
+}
